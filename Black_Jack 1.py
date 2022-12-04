0aaa64db6a780721aa4494e491b40300f026ce8c
Juhi Thakre <juhithakre394@gmail.com>
2022-12-04 12:40:14 +0530

    Added Black-Jack-Game

---
+++ Black_Jack 1.py
@@ -0,0 +1,84 @@
+import random
+playerIn = True
+dealerIn = True
+
+#deck of cards
+deck = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
+playerHand = []
+dealerHand = []
+
+#deal the card
+def dealCard(turn):
+    card = random.choice(deck)
+    turn.append(card)
+    deck.remove(card)
+
+#calcculate the total of each hand
+def total(turn):
+     total = 0
+     face = ['J', 'K', 'Q']
+     for card in turn:
+         if card in range(1, 11):
+             total += card
+         elif card in face:
+             total += 10
+         else:
+             if total > 11:
+                 total += 1
+             else:
+                 total += 11
+     return total
+
+
+ #check for winner
+def revealDealerHand():
+    if len(dealerHand) == 2:
+        return dealerHand[0]
+    elif len(dealerHand) > 2:
+        return dealerHand[0], dealerHand[1]
+
+
+ #game loop
+for _ in range(2):
+      dealCard(dealerHand)
+      dealCard(playerHand)
+
+while playerIn or dealerIn:
+      print(f"Dealer had {revealDealerHand()} and X")
+      print(f"you have {playerHand} for a total of {total(playerHand)}")
+      if playerIn:
+          stayOrHit = input("1: stay\n2: Hit\n")
+      if total(dealerHand) > 16:
+          dealerIn = False
+      else:
+          dealCard(dealerHand)
+      if  stayOrHit == '1':
+          playerIn = False
+      elif stayOrHit == '2':
+          dealCard(playerHand)
+      else:
+          print("put Input correctly")
+          continue
+      if total(playerHand) >= 21:
+          break
+      elif total(dealerHand) >= 21:
+          break
+
+if total(playerHand) == 21:
+      print(f"\nyou have {playerHand} for a total of {total(playerHand)} and total of {total(dealerHand)}")
+      print("BlackJack! you win!")
+elif total(dealerHand) == 21:
+      print(f"\nyou have {playerHand} for a total of {total(playerHand)} and total of {total(dealerHand)}")
+      print("BlackJack! Dealer win!")
+elif total(playerHand) > 21:
+      print(f"\nyou have {playerHand} for a total of {total(playerHand)} and total of {total(dealerHand)}")
+      print("you bust! dealer wins!")
+elif total(dealerHand) > 21:
+      print(f"\nyou have {playerHand} for a total of {total(playerHand)} and total of {total(dealerHand)}")
+      print("dealer bust! you wins!")
+elif 21 - total(dealerHand) < 21 - total(playerHand):
+      print(f"\nyou have {playerHand} for a total of {total(playerHand)} and total of {total(dealerHand)}")
+      print("Dealer wins!")
+elif 21 - total(dealerHand) > 21 - total(playerHand):
+      print(f"\nyou have {playerHand} for a total of {total(playerHand)} and total of {total(dealerHand)}")
+      print("you wins!")
